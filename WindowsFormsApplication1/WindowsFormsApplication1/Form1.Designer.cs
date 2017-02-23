33cd2401d77b539e6dda7dfe127f6d6e70116664
Bman <Bman@BMAN-PC>
2017-02-23 20:35:42 +0100

    pixel_rajzolás

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,11 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(93, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(443, 330);
+            this.panel2.TabIndex = 0;
+            this.panel2.Resize += new System.EventHandler(this.panel2_Resize);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 480);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(93, 348);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(443, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(635, 562);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,72 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        
+
+        Bitmap buffer;
+        Graphics bufferg;
+
+       
+        private void panel2_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+
+        Thread t;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            t = new Thread(new ThreadStart(szal));
+            t.Start();
+
+        }
+        void szal()
+        {
+
+            bufferg.Clear(Color.White);
+
+            int h, w;
+
+            lock (buffer)
+            {
+                h = buffer.Height;
+                w = buffer.Width;
+
+            }
+
+            for (int j = 0; j < h; j++)
+                for (int i = 0; i < w; i++)
+                    if ((j * w + i) % 8 == 1)
+                        lock (buffer)
+                            buffer.SetPixel(i, j, Color.Blue);
+
+            this.Invoke(new Action(() => { button1.Enabled = true; }));
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (buffer == null)
+                return;
+
+            using (Graphics g = panel2.CreateGraphics()) 
+            {
+
+                lock (buffer)
+                    g.DrawImage(buffer, 0, 0);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buffer = new Bitmap(panel2.Width, panel2.Height);
+            bufferg = Graphics.FromImage(buffer);
         }
     }
 }
